d998bb9961267625bce85f64ab6ebf1d82f190a8
takuro inoue <imuri.1g1g@gmail.com>
2020-03-07 05:55:00 +0000

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about',   to: 'landing_pages#about'
   get '/contact', to: 'landing_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
   
  root 'application#hello'
 end
