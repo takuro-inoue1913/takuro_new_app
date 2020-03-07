81cdd8d2c60ed4059c6b8f4ad434a0b0fe2f0e0a
takuro inoue <imuri.1g1g@gmail.com>
2020-03-07 03:21:26 +0000

    test valid or not valid email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,8 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Foobar", email: "sample@foobar.com")
   end
   
+  
+  
   test "should be valid" do
     assert @user.valid?
   end
@@ -18,5 +20,35 @@ class UserTest < ActiveSupport::TestCase
    test "email should be present" do
     @user.email = "  "
     assert_not @user.valid?
+  end
+  
+  test "testing name length" do
+    @user.name = "a" * 31
+    # 最大30文字
+    assert_not @user.valid?
+  end
+  
+  test "testing email length" do
+    @user.email = "a" * 188 + "@hogehoge.com"
+    # 最大200文字
+    assert_not @user.valid?
+  end
+  
+  test "test valid email" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect}を有効にしてください"
+    end
+  end
+  
+  test "test not valid email" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 end
