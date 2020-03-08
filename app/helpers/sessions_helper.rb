6b494bcaffd4b2e8ee20c770fa32f3593d918ee1
takuro inoue <imuri.1g1g@gmail.com>
2020-03-08 08:39:57 +0000

    log_out test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,4 +15,10 @@ module SessionsHelper
       !current_user.nil?
        # ログインしていたらture,していなかったらfalseを返す
   end
+  
+   # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
