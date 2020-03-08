8518f031f0db41488ff8d43d26468ad500c769f4
takuro inoue <imuri.1g1g@gmail.com>
2020-03-08 01:41:46 +0000

    session_controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'landing_pages#home'
   
   get '/help',    to: 'landing_pages#help'
   get '/about',   to: 'landing_pages#about'
   get '/contact', to: 'landing_pages#contact'
-  get '/signup',  to: 'users#new'
-  post '/signup', to: 'users#show'
+  
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#show'
+  
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'session#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
   
  root 'application#hello'
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get new" do
+    get login_path
+    assert_response :success
+  end
+  
+  test "should get create" do
+    post login_path
+    assert_response :success
+  end
+  
+  test "should get delete" do
+    get logout_path
+    assert_response :success
+  end
+
+end
