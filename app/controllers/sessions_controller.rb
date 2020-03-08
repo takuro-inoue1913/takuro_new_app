95624b120b56abfdf1559461a39883d161a6fcd4
takuro inoue <imuri.1g1g@gmail.com>
2020-03-08 06:05:30 +0000

    sessions_controller logintest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+  
+  def create
+    user = User.find_by(   email: params[:session][:email].downcase,
+                        username: params[:session][:username])
+                        
+    if user && user.authenticate(params[:session][:password])
+      
+    else
+      flash[:danger] = "入力が間違っています。"
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#show'
   
-  # get    '/login',  to: 'sessions#new'
-  # post   '/login',  to: 'sessions#create'
-  # delete '/logout', to: 'sessions#destroy'
-  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
