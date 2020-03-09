d7029a00d5b73d1e5aa5011252484fcfb4774faf
takuro inoue <imuri.1g1g@gmail.com>
2020-03-09 11:42:29 +0000

    forget

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,5 +41,8 @@ class User < ApplicationRecord
       BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
-  
+   # ユーザー情報を忘れる
+  def forget
+      update_attribute(:remember_digest, nil)
+  end
 end
