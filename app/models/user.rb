efc3d74fb353e34bb0e2bd478d12b0f1f83bcb07
takuro inoue <imuri.1g1g@gmail.com>
2020-03-09 13:14:09 +0000

    tweak file

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,8 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
-   # ユーザー情報を忘れる
+  # ユーザーのログイン情報を破棄する
   def forget
-      update_attribute(:remember_digest, nil)
+    update_attribute(:remember_digest, nil)
   end
 end
