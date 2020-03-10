93b3c05ed51646441fe8a234b3eeafb490141cf1
takuro inoue <imuri.1g1g@gmail.com>
2020-03-10 11:49:58 +0000

    user_model phone_number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200310113055) do
+ActiveRecord::Schema.define(version: 20200310114612) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20200310113055) do
     t.string "remember_digest"
     t.string "webpage"
     t.string "self_introduction"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
