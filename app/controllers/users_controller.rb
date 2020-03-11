6523eb865cf7b6cddd133474c94bd94c9c96ecec
takuro inoue <imuri.1g1g@gmail.com>
2020-03-11 12:27:46 +0000

    users_controller correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_users, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -55,4 +56,9 @@ class UsersController < ApplicationController
      end
    end
    
+   # 正しいユーザーかテスト
+   def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_url) if not @user == current_user
+   end
 end
