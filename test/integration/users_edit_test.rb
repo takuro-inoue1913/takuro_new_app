68e0c38e14860716f6ddf0543afec60b051f2aae
takuro inoue <imuri.1g1g@gmail.com>
2020-03-11 13:04:31 +0000

    friendly forwarding test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -21,24 +21,43 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
   
   
-  test "successful edit test" do 
-    # 編集失敗テスト
-    log_in_as(@user)
+  # test "successful edit" do
+  #   log_in_as(@user)
+  #   get edit_user_path(@user)
+  #   assert_template 'users/edit'
+  #   name  = "Foo Bar"
+  #   email = "foo@bar.com"
+  #   username = "hogehoge"
+  #   patch user_path(@user), params: { user: { name:  name,
+  #                                             email: email,
+  #                                             password:              "",
+  #                                             password_confirmation: "" ,
+  #                                             username: username
+  #   } }
+  #   assert_not flash.empty?
+  #   assert_redirected_to @user
+  #   @user.reload
+  #   assert_equal name,  @user.name
+  #   assert_equal email, @user.email
+  # end
+  
+  
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
-    name = "Foo bar"
-    username = "samle name"
-    email = "foo@bra.com"
-    patch user_path(@user), params: { user: { name: name,
-                                              username: username,
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    username = "hogehoge"
+    patch user_path(@user), params: { user: { name:  name,
                                               email: email,
+                                              username: username,
                                               password:              "",
-                                              password_confirmation: "" }}
+                                              password_confirmation: ""  } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert_equal name, @user.name
-    assert_equal username, @user.username
+    assert_equal name,  @user.name
     assert_equal email, @user.email
   end
 end
