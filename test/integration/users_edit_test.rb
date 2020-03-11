b18b68a842ca6ab8435e7c432ff0195c09a1e25b
takuro inoue <imuri.1g1g@gmail.com>
2020-03-11 10:16:25 +0000

    users_edit_test failures

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,5 +17,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password: "fo",
                                               password_confirmation: "bar" } }
     assert_template 'users/edit'
-  end 
+  end
+  
+  
+  test "successful edit test" do 
+    # 編集失敗テスト
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo bar"
+    username = "samle name"
+    email = "foo@bra.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              username: username,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" }}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal username, @user.username
+    assert_equal email, @user.email
+  end
 end
