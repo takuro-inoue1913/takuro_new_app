4fc74bbe1a40bd802906b18f4cfbf807f12c0b36
takuro inoue <imuri.1g1g@gmail.com>
2020-03-12 10:19:12 +0000

    userscontroller index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_users, only: [:edit, :update]
+  before_action :logged_in_users, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   
   
   
   def index
-     @user = User.all
+     @users = User.all
   end
   
   
