3a1db02193787a5ea9a08bda56abfcc538908959
takuro inoue <imuri.1g1g@gmail.com>
2020-03-13 09:22:20 +0000

    seed.rb admin: true users_controller_test

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@ User.create!(name:  "Example User",
              username: "smple user",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
