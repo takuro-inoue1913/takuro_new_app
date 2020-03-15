be47f9975b902d207f7fee9e9acc006dd7dfd752
takuro <imuri.1g1g@gmail.com>
2020-03-15 05:10:49 +0000

    test_user, seed_user activated,activated_at

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 User.create!(name:  "Example User",
              username: "smple user",
-             email: "foobar.@gmail.com",
+             email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar" ,admin: true)
+             password_confirmation: "foobar" ,
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -13,5 +16,7 @@ User.create!(name:  "Example User",
                email: email,
                username: username,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
