c3c1e241b6292e6b2a461a597690b1e7e6519537
takuro <imuri.1g1g@gmail.com>
2020-03-15 04:35:41 +0000

    users_column activation_digest, activated, :boolean, activated_at, :datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200313091017) do
+ActiveRecord::Schema.define(version: 20200315042918) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20200313091017) do
     t.string "phone_number"
     t.string "sex"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
