a8e709af80687108a31dab03175c6176f4b9866a
takuro <imuri.1g1g@gmail.com>
2020-03-16 13:27:38 +0000

    routes password_resets, session/new.html.erb password_reset link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
+  
   root 'landing_pages#home'
   
   get '/help',    to: 'landing_pages#help'
@@ -18,6 +15,6 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
-  
+  resources :password_resets, only: [:new, :edit, :create, :update]
  root 'application#hello'
 end
