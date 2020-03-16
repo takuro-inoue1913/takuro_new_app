9e48828e1617d1caa42852c146473b8a450dc9e3
takuro <imuri.1g1g@gmail.com>
2020-03-16 11:23:03 +0000

    sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,16 @@ class SessionsController < ApplicationController
                         username: params[:session][:username])
                         
     if user && user.authenticate(params[:session][:password])
-      log_in user
+      if user.activated?
+        log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or user
+      else
+        message = "アカウントが有効化されていません"
+        message += "送られているEmailを確認し、リンクをクリックしてください"
+        flash[:warning] = massage
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'User name, email, password いずれかが間違っています。'
       render 'new'
