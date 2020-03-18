a1359af1499fb1ccfa93c952433b2ee58bd11103
takuro <imuri.1g1g@gmail.com>
2020-03-18 13:27:48 +0000

    landing_pages_controller micropost => microposts, bulid => build

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,6 +1,6 @@
 class LandingPagesController < ApplicationController
   def home
-    @micropost = current_user.micropost.bulid if logged_in?
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
