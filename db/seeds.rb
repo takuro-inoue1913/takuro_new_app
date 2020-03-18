2c81cd954a795aeb1cc580fc2971d8b0483700d3
takuro <imuri.1g1g@gmail.com>
2020-03-18 11:19:39 +0000

    Faker::Lorem content

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,4 +19,11 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+
+
+users = User.order(:created_at).take(6)
+5.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
 end
