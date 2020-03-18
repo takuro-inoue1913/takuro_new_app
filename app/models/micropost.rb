818d42637dcdeef65c976753716147bf1f6248ff
takuro <imuri.1g1g@gmail.com>
2020-03-18 10:16:58 +0000

    micropost_model default_scope

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  # デフォルトの順序を指定する（降順）
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
