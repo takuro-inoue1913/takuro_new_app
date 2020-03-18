5ec549651f27eaf9ba63e8f831a79a8a4f8998d7
takuro <imuri.1g1g@gmail.com>
2020-03-18 13:09:33 +0000

    microposts_controller create_action, micropost_params

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,10 +2,21 @@ class MicropostsController < ApplicationController
   before_action :logged_in_users, only: [:create, :destroy]
     
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "投稿が完了しました！"
+      redirect_to root_url
+    else
+      render 'landing_pages/home'
+    end
   end
   
   def destroy
   end
     
-    
+  
+  def micropost_params
+      params.require(:micropost).permit(:content)
+  end
+  
 end
