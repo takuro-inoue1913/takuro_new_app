1b5b6445325465784256636f6b5b894f52bef617
takuro <imuri.1g1g@gmail.com>
2020-03-18 10:08:22 +0000

    micropoststest order should be most recent first, microposts.yml

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,8 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    # @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -25,4 +26,9 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
+  
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+  
 end
