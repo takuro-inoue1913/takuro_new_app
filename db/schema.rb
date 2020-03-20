aca54c51121752a25122f0d3f66397e6fb407fff
takuro <imuri.1g1g@gmail.com>
2020-03-20 06:39:17 +0000

    g migration add_image_to_users image:string => db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200320021926) do
+ActiveRecord::Schema.define(version: 20200320055638) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20200320021926) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
