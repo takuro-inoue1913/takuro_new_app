cb6ff3bbe6c7f441c2fc75f8cce130d09fcda295
takuro <imuri.1g1g@gmail.com>
2020-03-20 03:38:14 +0000

    micropost_model validate:

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,15 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+  
+  
+  private
+  
+  # アップロードされた画像のサイズをバリデーションする (上限 5MB)
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "5MB以上のdateは投稿できません")
+      end
+    end
 end
