7d12e1980e3a47bcf00a1d8f2613eedeb35095a7
takuro <imuri.1g1g@gmail.com>
2020-03-21 04:39:32 +0000

    user_model follow(other_user), unfollow(other_user), following?(other_user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -108,7 +108,29 @@ class User < ApplicationRecord
   end
   
   
+  
+  
+   # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+    # active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
+  
+  
   private
+  
+  
     
     def downcase_email
         self.email = email.downcase
