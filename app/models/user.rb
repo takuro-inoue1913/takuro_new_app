9953461739859553059abcf3b3fc82afd8846755
takuro <imuri.1g1g@gmail.com>
2020-03-21 05:01:25 +0000

    user_model has_many :passive_relationships, :followers  user_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,18 @@ class User < ApplicationRecord
     has_many :active_relationships,  class_name: "Relationship",
                                     foreign_key: "follower_id",
                                       dependent: :destroy
+    
+    has_many :passive_relationships, class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                      dependent: :destroy
                                     
     has_many :following, through: "active_relationships",
                           source: "followed"
     # == @user.active_relationships.map(&:followed)
+    
+    has_many :followers, through: "passive_relationships",
+                          source: "follower"
+    # == @user.passive_relationships.map(&:follower)
     
     has_many :microposts, dependent: :destroy
     
