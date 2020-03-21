70ac8095082f8dc321df2ebd74c5620e304156af
takuro <imuri.1g1g@gmail.com>
2020-03-21 04:24:57 +0000

    has_many :active_relationships dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :active_relationships,  class_name: "Relationship",
-                                    foreign_key: "follower_id"
+                                    foreign_key: "follower_id",
+                                      dependent: :destroy
                                     
     has_many :following, through: "active_relationships",
                           source: "followed"
