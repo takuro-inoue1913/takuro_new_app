ee104bdfc56b3737f0b2f6374dc2e4ba18d55b19
takuro <imuri.1g1g@gmail.com>
2020-03-22 06:06:49 +0000

    relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,17 @@
 class RelationshipsController < ApplicationController
     before_action :logged_in_users
+    
+  # POST /relationships
   def create
+      user = User.find(params[:followed_id])
+      current_user.follow(user)
+      redirect_to user
   end
   
+  # DELETE /relationships/:id
   def destroy
+      user = Relationship.find(params[:id]).followed
+      current_user.unfollow(user)
+      redirect_to user
   end 
 end
