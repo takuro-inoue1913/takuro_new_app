72238c738799f6088780160f01f8bc7a2bec5d5c
takuro <imuri.1g1g@gmail.com>
2020-03-24 10:38:54 +0000

    micropost.rb has_many :iine_users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :likes, dependent: :destroy
+  has_many :likes,      dependent: :destroy
+  has_many :iine_users, through: :likes, source: :user
   
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
@@ -19,6 +20,13 @@ class Micropost < ApplicationRecord
   # マイクロポストのいいねを解除する
   def uniine(user)
     likes.find_by(user_id: user.id).destroy
+  end
+  
+  
+  
+  # 現在のユーザーがいいねしてたらtrueを返す
+  def iine?(user)
+    iine_users.include?(user)
   end
   
   
