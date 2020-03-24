89370e57a6ee350002f670cae59d8651907e53f7
takuro <imuri.1g1g@gmail.com>
2020-03-24 09:19:04 +0000

    likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+    before_action :logged_in_users
+    
+    
+    
+    def create
+      @micropost = Micropost.find(params[:micropost_id])
+      @micropost.iine(current_user)
+    end
+    
+    
+
+    def destroy
+      @micropost = Like.find(params[:id]).micropost
+      @micropost.uniine(current_user)
+    end
+    
+    
 end
