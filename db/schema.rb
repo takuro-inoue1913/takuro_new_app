e1b1420fcf06c1d773d0efffc7d709e6a1ca1276
takuro <imuri.1g1g@gmail.com>
2020-03-24 11:07:27 +0000

    micropost_model likes_count: integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200324083905) do
+ActiveRecord::Schema.define(version: 20200324110025) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20200324083905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "likes_count", default: 0, null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
