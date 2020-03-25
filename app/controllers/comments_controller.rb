345a2aef34eae9810d1bdc323265bda3a0721f17
takuro <imuri.1g1g@gmail.com>
2020-03-25 16:06:35 +0000

    comments_controller.rb create, destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+    
+    
+  def create
+      
+  @micropost = Micropost.find(params[:micropost_id])
+  @comment = @micropost.comments.build(comment_params)
+  @comment.user = current_user
+    if @comment.save
+      redirect_to micropost_path(@micropost)
+    else
+      render "microposts/show"
+    end
+
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+    redirect_to  request.referrer || root_url
+  end
+
+
+
+  private 
+
+    def comment_params
+      params.require(:comment).permit(:body, :picture)
+    end
+
 end
