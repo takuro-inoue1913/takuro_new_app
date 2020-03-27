55eca253dd33c57ea540a662bf31a2cea958c4ee
takuro <imuri.1g1g@gmail.com>
2020-03-27 11:45:26 +0000

    relationship_mailer.rb follow_notification, unfollow_notification

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -1,2 +1,14 @@
 class RelationshipMailer < ApplicationMailer
+    
+    def follow_notification(user, follower)
+      @user = user
+      @follower = follower
+      mail to: user.email, subject: "#{@follower.name}さんがあなたをフォローしました"
+    end
+
+    def unfollow_notification(user, follower)
+      @user = user
+      @follower = follower
+      mail to: user.email, subject: "#{@follower.name}さんがあなたのフォローを外しました"
+    end
 end
