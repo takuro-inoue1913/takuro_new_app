eb3916b7f2c6fababa43b59ee205e3b9e4f7404a
takuro <imuri.1g1g@gmail.com>
2020-03-27 12:27:08 +0000

    relationship_mailer_test.rb

--- test/mailers/relationship_mailer_test.rb
+++ test/mailers/relationship_mailer_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class RelationshipMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "follow_notification" do
+    user = users(:archer)
+    follower = users(:lana)
+    mail = RelationshipMailer.follow_notification(user, follower)
+    assert_equal "#{follower.name}さんがあなたをフォローしました", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name, mail.body.encoded
+    assert_match follower.name, mail.body.encoded
+  end
+
+  test "unfollow_notification" do
+    user = users(:archer)
+    follower = users(:lana)
+    mail = RelationshipMailer.unfollow_notification(user, follower)
+    assert_equal "#{follower.name}さんがあなたのフォローを外しました", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name, mail.body.encoded
+    assert_match follower.name, mail.body.encoded
+  end
 end
