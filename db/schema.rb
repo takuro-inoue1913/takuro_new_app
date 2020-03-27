5cf6f746132f757876f7f3fcf9a25bf5766e2af8
takuro <imuri.1g1g@gmail.com>
2020-03-27 12:23:57 +0000

    user_model follow_notification_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200324131841) do
+ActiveRecord::Schema.define(version: 20200327115559) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20200324131841) do
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.string "image"
+    t.boolean "follow_notification", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
