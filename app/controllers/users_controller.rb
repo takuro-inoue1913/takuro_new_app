29cab211731bd716e8feec5ed4bc8a50c13810bc
takuro <imuri.1g1g@gmail.com>
2020-03-29 11:43:21 +0000

    users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,24 @@ class UsersController < ApplicationController
   end
   
   
+  def facebook_login
+  @user = User.from_omniauth(request.env["omniauth.auth"])
+    result = @user.save(context: :facebook_login)
+    if result
+      log_in @user
+      redirect_to @user
+    else
+      redirect_to auth_failure_path
+    end
+  end
+
+#認証に失敗した際の処理
+def auth_failure 
+  @user = User.new
+  render 'new'
+end
+  
+  
   private
   
    def user_params
